28538c25209207b35bcd88454aea5aedad742107
bobbsoap <bobbsoap@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2010-12-23 18:39:41 +0000

    styled NameYourPrice

---
+++ Application/trunk/Applications/BevoMedia/Modules/Offers/Views/NameYourPriceGetResult.view.php
@@ -0,0 +1,4 @@
+<?php
+	sleep(4);
+	echo 'Success';
+	die();
--- Application/trunk/Applications/BevoMedia/Modules/Offers/Views/NameYourPriceResult.view.php
+++ Application/trunk/Applications/BevoMedia/Modules/Offers/Views/NameYourPriceResult.view.php
@@ -1,22 +1,27 @@
 <div id="pagemenu">
-	
+	<ul>
+		<li><a href="/BevoMedia/Offers/Index.html">&laquo; Back to Offers<span></span></a></li>
+		<li><a href="/BevoMedia/Offers/NameYourPrice.html">Name Your Payout<span></span></a></li>
+	</ul>
+	<ul class="floatright">
+		<li><a class="active" href="#">Your Results<span></span></a></li>
+	</ul>
 </div>
 
-<?php echo $this->PageDesc->ShowDesc($this->PageHelper, false); //2nd param to hide toggle btn, as there is nothing else on this page ?>
+<?php echo $this->PageDesc->ShowDesc($this->PageHelper, false); //2nd param to hide toggle btn, as there is nothing else on this page 
+?>
 
-
-
-	<div style="text-align: center;">
-
-	<?php echo $this->NetworkName?> has been elected the winning bidder. A Bevo Representative will email you to
-	confirm your negotiated payout and get you in touch with your <?php echo $this->NetworkName?>
-	Representative. This process can take up 24 hours.
+<div class="pagecontent nyp aligncenter">
+	<h3>Success!</h3>
+	<p>We have found a match for your desired payout. The winning network is <?php echo $this->NetworkName?>:</p>
+	<br />
 	
+	<a href="/BevoMedia/Publisher/ApplyAdd.html?network=<?php echo $this->AffNetworkID?>" rel="shadowbox;width=640;height=480;player=iframe" >
+		<img class="nwpic small" src="/Themes/BevoMedia/img/networklogos/uni/<?php echo $this->AffNetworkID ?>.png" />
+	</a>
+	<a class="btn nw_applyadd" href="/BevoMedia/Publisher/ApplyAdd.html?network=<?php echo $this->AffNetworkID?>" rel="shadowbox;width=640;height=480;player=iframe" >Apply/add</a>
+	<a href="/BevoMedia/Publisher/ApplyAdd.html?network=<?php echo $this->AffNetworkID?>" rel="shadowbox;width=640;height=480;player=iframe" >Click to join this network now</a>
 	<br /><br />
 	
-	<img class="nwpic small" src="/Themes/BevoMedia/img/networklogos/uni/<?php echo $this->AffNetworkID ?>.png" /><br /><br />
-	
-	<a href="/BevoMedia/Publisher/ApplyAdd.html?network=<?php echo $this->AffNetworkID?>" rel="shadowbox;width=640;height=480;player=iframe" >Apply/Add this network</a>
-	
-	</div>
-	
+	<p>A Bevo representative will email you to confirm your negotiated payout and get you in touch with your <?php echo $this->NetworkName?> representative. This process can take up to 24 hours.</p>
+</div>
