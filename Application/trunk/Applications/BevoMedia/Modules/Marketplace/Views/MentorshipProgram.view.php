6baede2d583824ab5b21e87749a960b3a37a61d2
imitrik <imitrik@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2011-01-20 20:19:16 +0000

    mentorship

--- Application/trunk/Applications/BevoMedia/Modules/Marketplace/Views/MentorshipProgram.view.php
+++ Application/trunk/Applications/BevoMedia/Modules/Marketplace/Views/MentorshipProgram.view.php
@@ -2,8 +2,6 @@
 	<ul>
 		<li><a href="/BevoMedia/Marketplace/">Marketplace<span></span></a></li>
 		<li><a class="active" href="/BevoMedia/Marketplace/MentorshipProgram.html">Mentorship Program<span></span></a></li>
-		<?php if($this->User->membershipType != 'premium')
-			echo '<li><a href="/BevoMedia/Marketplace/Premium.html">Get Bevo Premium<span></span></a></li>';?>
 	</ul>
 </div>
 <?php echo $this->PageDesc->ShowDesc($this->PageHelper); ?>
