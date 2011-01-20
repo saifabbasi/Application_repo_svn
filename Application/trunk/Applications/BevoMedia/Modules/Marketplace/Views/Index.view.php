af48eb577a0b77effde504ee87c3196460221944
imitrik <imitrik@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2011-01-20 20:19:29 +0000

    metorship

--- Application/trunk/Applications/BevoMedia/Modules/Marketplace/Views/Index.view.php
+++ Application/trunk/Applications/BevoMedia/Modules/Marketplace/Views/Index.view.php
@@ -172,8 +172,7 @@ $strPageHead = '<script language="Javascript" src="js/jquery-1.3.1.min.js"></scr
 		<ul>
 			<li><a class="active" href="/BevoMedia/Marketplace/">Marketplace<span></span></a></li>
 			<li><a href="/BevoMedia/Marketplace/MentorshipProgram.html">Mentorship Program<span></span></a></li>
-			<?php if($this->User->membershipType != 'premium')
-				echo '<li><a href="/BevoMedia/Marketplace/Premium.html">Get Bevo Premium<span></span></a></li>';?>
+			
 		</ul>
 	</div>
 	<?php echo $this->PageDesc->ShowDesc($this->PageHelper); ?>
