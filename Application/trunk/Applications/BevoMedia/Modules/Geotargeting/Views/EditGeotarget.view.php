415ccf09671d6cfe89d86fe717f8b5468cfdf37a
imitrik <imitrik@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2011-01-27 21:24:56 +0000

    double url encode

--- Application/trunk/Applications/BevoMedia/Modules/Geotargeting/Views/EditGeotarget.view.php
+++ Application/trunk/Applications/BevoMedia/Modules/Geotargeting/Views/EditGeotarget.view.php
@@ -37,7 +37,7 @@
 	<div style="font-weight: bold; font-size: 14px;">Geotarget Code</div>
 	<div>Please place this coding on a blank php file. Viewers will then be redirected accordingly when they access the page.</div>
 	<textarea class="code" style="width: 100%; margin: 0px; line-height: 20px; padding: 0px; padding-top: 20px;" wrap="off"><?php 
-			$Code = "<script type=\"text/javascript\" src=\"http://track.bevomedia.com/geo/{$_GET['ID']}/{$this->User->id}/".'<?=$_SERVER["REMOTE_ADDR"]?>/<?=urlencode(base64_encode(gzcompress(serialize($_GET),9)))?>'."\"></script>";
+			$Code = "<script type=\"text/javascript\" src=\"http://track.bevomedia.com/geo/{$_GET['ID']}/{$this->User->id}/".'<?=$_SERVER["REMOTE_ADDR"]?>/<?=urlencode(urlencode(base64_encode(gzcompress(serialize($_GET),9))))?>'."\"></script>";
 			echo htmlentities($Code);
 		?></textarea>
 	
