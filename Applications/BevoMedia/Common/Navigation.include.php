1e890de28a4d95a9933041fc44b70e3364bedf37
bvolchik <bvolchik@rcs.co>
2011-02-22 21:41:29 +0000

    Geographics menu updated

--- Applications/BevoMedia/Common/Navigation.include.php
+++ Applications/BevoMedia/Common/Navigation.include.php
@@ -132,6 +132,7 @@
 						),
 					'geotargeting' => array('Geographics','/BevoMedia/Geotargeting/Index.html', 
 						array(
+							'geotargeting_new'=>array('Add Geotarget', '/BevoMedia/Geotargeting/NewGeotarget.html'),
 							'geotargeting_existing'=>array('Existing Geotargets','/BevoMedia/Geotargeting/Index.html'),
 							'geotargeting_help'=>array('Geotargetting Help','/BevoMedia/Publisher/PPCTutorialsKWTracker.html#HowToUseGeotargeting'),
 							'geoparting'=>array('Geoparting','/BevoMedia/KeywordTracker/Geoparting.html'),
