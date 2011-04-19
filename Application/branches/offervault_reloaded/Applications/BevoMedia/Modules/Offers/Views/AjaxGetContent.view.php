499262447a76200f9bf4726e18f64e4815b3ea6a
bobbsoap <bobbsoap@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2011-04-19 21:17:17 +0000

    offervault reloaded branch, partially ready to be implemented with DB

---
+++ Application/branches/offervault_reloaded/Applications/BevoMedia/Modules/Offers/Views/AjaxGetContent.view.php
@@ -0,0 +1,80 @@
+<?php /* it's soapdesigned.com */
+
+/** NOT AN OUTPUT PAGE!
+  * This script accepts GET params via Ajax, fetches the XML object from PPVSpy via API, and returns the correct content.
+  * It should never be loaded in the browser directly.
+  *
+  * Todo at a later date since this file is loaded thru Ajax:
+  *	To prevent abuse, load session & app header and check if user is logged in and verified (and is allowed to use this feature)
+  *	Maybe keep the overhead as small as possible and not load the entire app header with all classes.
+  *	There is currently NO PROTECTION on this page!
+  *
+  */
+
+  
+/* ########################## load session and add user check here ############################## */
+
+
+/*set statics*/
+//array of the allowed GET['page'] params. param is added in ajax: it's a token for the API's page name.
+$allowed_gets = array('orowbig', 'searchresults');
+$query = array();
+$out = array();
+$error = false;
+
+/*start script*/
+if($_GET && isset($_GET['get']) && in_array($_GET['get'], $allowed_gets)) {
+	
+	$query['get'] = htmlspecialchars(trim($_GET['get']));
+
+	switch($query['get']) {
+		case 'orowbig' : //tr.orowbig for offer details
+			if(isset($_GET['oid']) && !empty($_GET['oid']) && is_numeric($_GET['oid']))
+				$query['params']['oid'] = intval(trim($_GET['oid']));
+			else	$error = 'The offer you have requested doesn\'t seem to exist. Please try again!';
+		break;
+		
+		case 'searchresults' :
+			$required_keys = array('search','type','include_networks');
+			
+			foreach($required_keys as $key) {
+				$f = str_replace(array('\'','"'), '', strip_tags(trim($_GET[$key])));
+				
+				//LATERRRRRR check for values like "1000,2000,,,3000,,4000": strip out empty
+				
+				if($f != '')
+					$query['params'][$key] = $f;
+				else	$error = 'You have either entered an invalid search term, or a required option is missing. Please try again!'; //only 1 error for all
+			}
+		break;
+		
+	}//end switch
+	
+	if($error) {
+		$out['error'] = $error;
+	
+	} elseif(!empty($query['params'])) {
+			
+		//build request
+		include_once dirname(__FILE__).'/Ovault.ConstructAjaxOutput.class.php';
+		$construct = new ConstructAjaxOutput();
+		
+		$out = $construct->$query['get']($query);		
+	}
+}//endif GET
+
+/*output*/
+//if(!array_key_exists('html', $out) || !array_key_exists('error', $out))
+if(count($out) == 0) {
+	$out['error'] = 'Oops, something went wrong! Please try again.';
+
+//elseif(array_key_exists('html', $out))
+//	echo $out['html'];
+} else {
+	//construct sanitized search string and add it
+	//$out['searchstring'] = LATERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRR 
+	
+	echo json_encode($out);
+}
+
+die(); //figuring this line out took a while.
---
+++ Application/branches/offervault_reloaded/Applications/BevoMedia/Modules/Offers/Views/Ovault.Odial.include.php
@@ -0,0 +1,153 @@
+<div id="odial">
+	<?php /*
+
+	THIS IS THE ENHANCED ODIAL, including country selection and media type options.
+	It's already fully CSS'd, but not yet JS'd.
+	For now, we are using the SIMPLE .odial .top below this one. It has separate CSS rules.
+	Once we implement countries and media type, we can bring this one back and remove the simple one.
+	
+	################ START enhanced top
+	
+	<div class="top">
+		<div class="label_searchfor"></div>
+		
+		<div class="main">
+			<div class="obox upper">
+				<label for="osearch">any offer name or vertical...</label>
+				<input type="text" class="formtxt" id="osearch" name="osearch" value="any offer name or vertical.." />
+				<a class="btn ocheck ocheck_lead active" href="#" data-key="type" data-value="lead">Lead</a>
+				<a class="btn ocheck ocheck_sale" href="#" data-key="type" data-value="sale">Sale</a>
+				<div class="clear"></div>
+			</div><!--close obox upper-->
+			
+			<div class="obox mediatype">
+				<a class="btn ocheck ocheck_display active" href="#" data-key="media" data-value="display">Display</a>
+				<a class="btn ocheck ocheck_search active" href="#" data-key="media" data-value="search">Search</a>
+				<a class="btn ocheck ocheck_email" href="#" data-key="media" data-value="email">Email</a>
+				<a class="btn ocheck ocheck_other" href="#" data-key="media" data-value="other">Other</a>
+				<div class="clear"></div>
+			</div><!--close obox mediatype-->
+			
+			<div class="obox countries">
+				<div class="number j_showolay" id="number_countries" data-olay="olay_countries">1</div>
+				<div class="preview">
+					<ul>
+						<li>USA</li>
+					</ul>
+				</div>
+				<div class="drop"></div>
+			</div><!--close obox countries-->
+			
+			<div class="obox include">
+				<a class="btn ocheck ocheck_mysaved active" href="#" data-value="mysaved">My Saved Offers</a>
+				<div class="number j_showolay" id="number_networks" data-olay="olay_networks">0</div>
+			</div><!--close obox include-->
+			<div class="clear"></div>
+		</div><!--close main-->
+		
+		<a class="btn odial_go" href="#">Go</a>
+		
+	</div><!--close top-->
+	
+	############## END enhanced top
+	
+	*/ ?>
+	
+	<div class="top simple">
+		<div class="label_searchfor"></div>
+		
+		<form id="osearchform" method="post" action="">
+			<div class="main">
+				<div class="obox upper">
+					<label for="osearch">any offer name or vertical...</label>
+					<input type="text" class="formtxt" id="osearch" name="osearch" value="any offer name or vertical..." />
+					
+					<input type="hidden" id="osearch_type" name="type" value="lead" />
+					<input type="hidden" id="osearch_include_mysaved" name="include_mysaved" value="1" />
+					<input type="hidden" id="osearch_include_networks" name="include_networks" value="<?php /* 1000,1002,1004,1006,1008,1010*/?>" />
+					
+					<div class="clear"></div>
+				</div><!--close obox upper-->
+				
+				<div class="obox convtype">
+					<a class="btn ocheck ocheck_lead active" href="#" data-hiddenfield="type" data-value="lead">Lead</a>
+					<a class="btn ocheck ocheck_sale" href="#" data-hiddenfield="type" data-value="sale">Sale</a>
+					<div class="clear"></div>
+				</div><!--close obox mediatype-->
+				
+				<div class="obox include">
+					<a class="btn ocheck ocheck_mysaved active" href="#" data-hiddenfield="include_mysaved" data-value="0">My Saved Offers</a>
+					<div class="clear"></div>
+				</div><!--close obox countries-->
+				
+				<div class="obox networks">
+					<div class="number j_showolay" id="number_networks" data-olay="olay_networks">0</div>
+					<div class="clear"></div>
+				</div><!--close obox include-->
+				<div class="clear"></div>
+			</div><!--close main-->
+		
+			<!--<a class="btn odial_go" href="#">Go</a>-->
+			<input type="submit" class="btn formsubmit odial_go" value="Go" />
+		</form>
+		
+	</div><!--close top.simple-->
+	<div class="butt">
+		<div class="rows">
+			<div class="selebtn" href="#">200<span class="down"></span></div>
+			<div class="selebtn hide">
+				<a href="#" data-value="25">25</a>
+				<a href="#" data-value="50">50</a>
+				<a href="#" data-value="100">100</a>
+				<a class="active" href="#" data-value="200">200</a>
+			</div>
+		</div>
+		<div class="save">
+			<div class="selebtn" href="#">My Offers (2 Feb 2011)<span class="down"></span></div>
+			<a class="btn ovault_saveallpage" href="#">Save All Of This Page</a>
+		</div>
+	</div><!--close butt-->
+	
+	<!-- olay_networks -->
+	<div id="olay_networks" class="ovault_olay simple"><?php /*ODIAL TOP SIMPLE: when we switch to the normal/enhanced dial, also remove the class "simple" from this!*/ ?>
+		<div class="olaytop"></div>
+		
+		<div class="olaycont">
+			<a class="btn ovault_olay_close" href="#">Close</a>
+			<p>Select Networks you want to include in the search:</p>
+			
+			<div class="olaybox col2">
+				<div class="olayboxtitle allnetworks"></div>
+				<div class="olayboxstripe">
+					<a class="btn ovault_smallyell_all j_olay_selelist" href="#" data-hiddenfield="include_networks" data-ul="j_olay_allnetworkslist" data-action="addall" data-number="number_networks">All</a>
+					<a class="btn ovault_smallyell_none j_olay_selelist" href="#" data-hiddenfield="include_networks" data-ul="j_olay_allnetworkslist" data-action="removeall" data-number="number_networks">None</a>
+				</div>
+				<ul class="olay_selelist j_olay_allnetworkslist">
+					<?php	//list all networks
+					$fakenetworks = array('CPA Staxx','Clickbooth','Convert2Media','Wolf Storm Media','W4','Diablo Media');
+					
+					for($i=1; $i<=3; $i++) {
+						for($j=0; $j<=count($fakenetworks)-1; $j++) {
+							echo '<li><a href="#" data-hiddenfield="include_networks" data-value="1000" data-number="number_networks">'.$fakenetworks[$j].'</a></li>';
+						}
+					} ?>
+				</ul>
+			</div>
+			<div class="olaybox col1 nomargin">
+				<div class="olayboxtitle mynetworks"></div>
+				<div class="olayboxstripe">
+					<a class="btn ovault_smallyell_all j_olay_selelist" href="#" data-hiddenfield="include_networks" data-ul="j_olay_mynetworklist" data-action="addall" data-number="number_networks">All</a>
+					<a class="btn ovault_smallyell_none j_olay_selelist" href="#" data-hiddenfield="include_networks" data-ul="j_olay_mynetworklist" data-action="removeall" data-number="number_networks">None</a>
+				</div>
+				<ul class="olay_selelist j_olay_mynetworklist">
+					<?php for($j=0; $j<=count($fakenetworks)-1; $j++) {
+						echo '<li><a class="active" href="#" data-hiddenfield="include_networks" data-value="1000" data-number="number_networks">'.$fakenetworks[$j].'</a></li>';
+					} ?>
+				</ul>
+			</div>
+			<div class="clear"></div>
+		</div><!--close olaycont-->
+	
+		<div class="olaytopflag"></div>
+	</div><!--close #olay_networks-->
+</div><!--close #odial-->
