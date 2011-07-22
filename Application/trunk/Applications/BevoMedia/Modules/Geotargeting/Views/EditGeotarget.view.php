6762ca0555ca4fa9fc0bdc2205fb29517e352a1f
imitrik <imitrik@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2011-07-22 17:07:35 +0000

    Bug 2623

--- Application/trunk/Applications/BevoMedia/Modules/Geotargeting/Views/EditGeotarget.view.php
+++ Application/trunk/Applications/BevoMedia/Modules/Geotargeting/Views/EditGeotarget.view.php
@@ -78,7 +78,7 @@
 		          address,
 		          function(point) {
 		            if (!point) {
-			            console.debug(address + " not found");
+
 		            } else {
 		              var marker = new GMarker(point);
 		              map.addOverlay(marker);
