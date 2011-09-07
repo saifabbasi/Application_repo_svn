390a5cf24194a284a7712e72442399fd15ad4110
imitrik <imitrik@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2011-09-07 13:27:57 +0000

    Bug 2765

--- Application/trunk/Applications/BevoMedia/Modules/Publisher/Views/EditNetwork.view.php
+++ Application/trunk/Applications/BevoMedia/Modules/Publisher/Views/EditNetwork.view.php
@@ -87,7 +87,7 @@
 	</tr>
 	
 	<?php endif; ?>
-	<?php if ( ($this->Network->passwordLabel != '') && (!in_array($this->Network->id, array(1028, 1038, 1044, 1070, 1062, 1064, 1069, 1072, 1073, 1076, 1077, 1078))) ): ?>
+	<?php if ( ($this->Network->passwordLabel != '') && (!in_array($this->Network->id, array(1028, 1038, 1044, 1070, 1062, 1064, 1069, 1072, 1073, 1076, 1077, 1078, 1080))) ): ?>
 	<tr>
 		<th><?php print htmlentities($this->Network->passwordLabel); ; ?></th>
 		<td>
