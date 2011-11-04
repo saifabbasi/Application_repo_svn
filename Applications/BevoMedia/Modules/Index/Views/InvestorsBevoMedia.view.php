5212670ad5b148a277c5c5c4abd89f048827501f
imitrik <imitrik@rcs.co>
2011-11-04 15:59:21 +0000

    tag fix

--- Applications/BevoMedia/Modules/Index/Views/InvestorsBevoMedia.view.php
+++ Applications/BevoMedia/Modules/Index/Views/InvestorsBevoMedia.view.php
@@ -4,5 +4,9 @@
 	<div class="clear"></div>
 	
 	<div class="content"> 
-<p>BeVo Media LLC is funded entirely by its founders and partners. If you would like to be considered to help us achieve our long term goals as an investor, please contact us at <strong><a href="mailto:investors@bevomedia.com">investors@bevomedia.com<a></strong></p>
+		<p>BeVo Media LLC is funded entirely by its founders and partners. If you would like to be considered to help us achieve our long term goals as an investor, please contact us at 
+			<strong>
+				<a href="mailto:investors@bevomedia.com">investors@bevomedia.com</a>
+			</strong>
+		</p>
 	</div>
--- Applications/BevoMedia/Modules/Index/Views/Whyfree.view.php
+++ Applications/BevoMedia/Modules/Index/Views/Whyfree.view.php
@@ -36,7 +36,7 @@ our interface as free to the user as possible, and feel that the usage revenue m
 beneficial, on both ends.
 
 
-<br><br> 
+<br /><br /> 
 
 Additionally, Bevo Media is supported by our partnered networks, who pay a yearly fee to be on our
 
@@ -54,22 +54,22 @@ Media provide a top tier platform to better the internet marketing industry.
 <h3>How Do I Know My Data is Secure?</h3>
 <p>
 We go above and beyond to make sure your data is 100% secure. Your privacy is of the utmost importance to us. We are well aware that one of the fastest ways our business could fail would be if we were careless about our data security. Before I go forth and tell you all of our methods to ensure your security, the most important aspect to this topic is... 
- <br><br />
+ <br /><br />
  
 Simply put - If you have any ounce of worry about your data security, just use our Self Hosted version! 
- <br><br />
+ <br /><br />
  
 We offer our self hosted version for this very reason. With our self hosted version, your data is stored on your own local machine and databases, ensuring that you are the only person with access to your data. Our regular hosted version of BeVo Media is mainly targeted for our beginner and cost sensitive publishers, while our self hosted version was designed for our experienced high volume, security concerned publishers. We have worked hard to ensure that all of the bases are covered within this issue. 
- <br><br />
+ <br /><br />
  
 Likewise, we have done every step humanly possible to ensure that your data is secure on our hosted version, mainly by creating a legal privacy policy. 
- <br><br />
+ <br /><br />
  
 Our privacy policy clearly states that no keyword or traffic source related information will be used or observed by anyone on the Bevo Media team. It is not required for us to have a privacy policy, however, if a company does have one, they must abide by it to the fullest extent. Violating your own privacy policy is major security offense and is considered a federal violation by the state of law. We would not have created such a strict privacy policy if we didn't truly comply with what it states, and put ourselves at risk for no reason. That being said, I thought it was important to point out these rules to our users, and make it clear that we do, in fact, have a legal obligation to make sure their data is secure. If you take a look at some of our competitor's privacy policies, they do not address this issue, meaning they are technically free to do as they want with your data. BeVo Media, on the other hand, goes above and beyond to ensure our publishers have the reassurance that their data is safe.
- <br><br />
+ <br /><br />
 If there are any questions, or any other ideas of how we can prove to 
 our users that their data is 100% safe, please email me personally at <strong><a href="mailto:ryan@bevomedia.com" target="_blank">ryan@bevomedia.com</a></strong>.  Your security is of the utmost importance 
-to our company. <br> <br>
+to our company. <br /> <br />
 </p>
 
 	</div>
