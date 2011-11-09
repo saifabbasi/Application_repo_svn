f74f61f770e71c82c204fe50fd8a4c3c55ec3ebb
imitrik <imitrik@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2011-11-09 13:19:49 +0000

    Bug 2823

---
+++ Application/trunk/Applications/BevoMedia/Common/Network_Classes/RevenueAds.php
@@ -0,0 +1,60 @@
+<?php 
+
+require_once('CakeMarketing.Abstract.php');
+
+/**
+ * RevenueAds.php
+ *
+ * @category   RCS Framework 
+ * @package    Modules
+ * @subpackage Bevomedia
+ * @copyright  Copyright (c) 2009 RCS
+ * @author RCS
+ * @version 0.1.2
+ */
+class RevenueAds Extends CakeMarketingAbstract {
+	/**
+	 * @access protected
+	 * @var string $apiUrl
+	 */
+	protected $apiUrl = 'http://login.revenueads.com/affiliates/api/1/reports.asmx/Clicks';
+	
+	/**
+	 * @access protected
+	 * @var string $apiUrlConversions
+	 */
+	protected $apiUrlConversions = 'http://login.revenueads.com/affiliates/api/1/reports.asmx/Conversions';
+	
+	/**
+	 * @access protected
+	 * @var string $offersUrl
+	 */
+	protected $offersUrl = 'http://login.revenueads.com/affiliates/api/1/offers.asmx/OfferFeed';
+	
+	/**
+	 * @access protected
+	 * @var string $loginUrl
+	 */
+	protected $loginUrl = 'http://login.revenueads.com/login.php';
+	
+	/**
+	 * @access protected
+	 * @var string $offersAffiliateId
+	 */
+	protected $offersAffiliateId = '12011';
+	
+	/**
+	 * @access protected
+	 * @var string $offersApiKey
+	 */
+	protected $offersApiKey = 'C1sgh0JE';
+	
+	/**
+	 * Convert2Media class constructor.
+	 *
+	 */
+	Public Function __construct()
+	{
+
+	}
+}
