1aed321a2932edf92cc60769c27d633366910db2
imitrik <imitrik@978880a4-239e-43f3-ae2a-4b86eb5b7aa6>
2012-03-23 13:03:46 +0000

    Bug 3185

--- Application/trunk/Themes/BevoMedia/apps-layout.php
+++ Application/trunk/Themes/BevoMedia/apps-layout.php
@@ -460,6 +460,9 @@ $(document).ready(function () {
 //]]>
 </script>
 
+<?php 
+	if (!isset($_SERVER['HTTPS'])) {
+?>
 <script type="text/javascript">
 var sc_project=7778761; 
 var sc_invisible=1; 
@@ -467,7 +470,9 @@ var sc_security="ce27f004";
 </script>
 <script type="text/javascript" src="http://www.statcounter.com/counter/counter.js"></script>
 <noscript><div class="statcounter"><a title="tumblr pagecounter" href="http://statcounter.com/tumblr/" target="_blank"><img class="statcounter" src="http://c.statcounter.com/7778761/0/ce27f004/1/" alt="tumblr page counter"></a></div></noscript>
-
+<?php 
+	}
+?>
 <script type="text/javascript">
 var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
 document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
