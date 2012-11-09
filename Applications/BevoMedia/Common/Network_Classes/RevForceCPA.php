4ed78129829bb1fe02870d12b3fac653fc7cc325
imitrik <imitrik@rcs.co>
2012-11-09 13:23:10 +0000

    Bug 4213

---
+++ Applications/BevoMedia/Common/Network_Classes/RevForceCPA.php
@@ -0,0 +1,49 @@
+<?php 
+
+require_once('HasOffers.Abstract.php');
+
+/**
+ * MediaForce
+ *
+ * @category   RCS Framework 
+ * @package    Modules
+ * @subpackage Bevomedia
+ * @copyright  Copyright (c) 2010 RCS
+ * @author RCS
+ * @version 0.0.1
+ */
+class WowTrack Extends HasOffersAbstract {
+	/**
+	 * @access protected
+	 * @var string $apiUrl
+	 */
+	protected $offersApiKey = 'AFFva4HDaBDcsXXSP55QazuvXqaUy5';
+	
+	/**
+	 * @access protected
+	 * @var string $statsApiUrl
+	 */
+	protected $statsApiUrl = 'http://affiliates.revforcecpa.com/stats/stats.json';
+	
+	/**
+	 * @access protected
+	 * @var string $conversionsApiUrl
+	 */
+	protected $conversionsApiUrl = 'http://affiliates.revforcecpa.com/stats/lead_report.json';
+	
+	/**
+	 * @access protected
+	 * @var string $offersApiUrl
+	 */
+	protected $offersApiUrl = 'http://affiliates.revforcecpa.com/offers/offers.json';
+	
+	
+	/**
+	 * EWA class constructor.
+	 *
+	 */
+	Public Function __construct()
+	{
+		$this->networkId = 1104;
+	}
+}
